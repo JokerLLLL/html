2fb6a145a95a5e1b022b48f671fb2b64ebbfa80c
home <home@test.com>
2019-02-06 00:57:01 +0800

    瀑布流布局

--- 18monster/index.js
+++ 18monster/index.js
@@ -81,19 +81,11 @@ var jquery_waterfall = function (wrap,boxes) {
             heights_array[$i] = boxes.eq($i).height() + 40;
         }else{
             //绝对定位
-            console.log(heights_array);
             var min_height = Math.min.apply(null,heights_array);
             var min_index = getIndex(min_height,heights_array);
             var leftValue = boxes.eq(min_index).position().left;
             //对 i个 box 进行定位 //增加 1s 动画效果
-            boxes.eq($i).css({
-                'position':'absolute',
-                'top':min_height,
-                'left':leftValue,
-                'opacity':0,
-            }).stop().animate({
-                opacity:1
-            },1000);
+            setOneBoxStyle(boxes.eq($i),min_height,leftValue,$i);
             //维护最小值
             heights_array[min_index] += boxes.eq($i).height() + 40;
         }
@@ -102,12 +94,79 @@ var jquery_waterfall = function (wrap,boxes) {
 
 }
 
-$(document).ready(function(){
+//专门设置盒子位置的定位函数
+var startIndex = 0;
+var setOneBoxStyle = function(box,top,left,index) {
+    // 这里是过滤已经追加过样式的盒子
+    if(index <= startIndex) {
+        console.log('xxx');
+        return false;
+    }
+    box.css({
+        'position':'absolute',
+        'top':top,
+        'left':left,
+        'opacity':0,
+    }).stop().animate({
+        opacity:1
+    },1000);
+    startIndex = index;
+}
+
+var getHtml = function () {
+    var getDatas = [{
+        "image":"img/5.jpg",
+        "name":"第一怪"
+    },{
+        "image":"img/3.jpg",
+        "name":"第一怪"
+    },{
+        "image":"img/7.jpg",
+        "name":"第一怪"
+    },{
+        "image":"img/1.jpg",
+        "name":"第一怪"
+    }];
+    var ret = '';
+    for (i=0;i<getDatas.length;i++) {
+        ret += '<div><img src="'+getDatas[i].image+'" alt=""><a href="#">'+getDatas[i].name+'</a></div>'
+    }
+    return ret;
+}
+
+var appendBox = function (wrap,boxes) {
+    var html = getHtml();
+    wrap.append(html);
+    //重新开始调用
+    jquery_waterfall(wrap, wrap.children('div'));
+}
+//检查是否滚到最后面
+function checkAppend(wrap){
+    //当前窗口显示的文档高度
+    var docHeight = $(window).height();
+    //文档滚动高度 (指当前窗口 距离 top的值 滚轮位置 px)
+    var scrollHeight = $(window).scrollTop();
+
+    //最后一个盒子所在高度的总高度
+    var boxes = wrap.children('div');
+    var lastBox = boxes.eq(boxes.length -1);
+    var lastTopHeight = lastBox.offset().top;
+    var lastColHeight = lastBox.height() + 20;
+    var lastBoxHeight = lastTopHeight + lastColHeight;
+    console.log(lastTopHeight,lastColHeight,docHeight, scrollHeight)
+    return (docHeight + scrollHeight) >= lastBoxHeight ? true : false;
+}
+
+$(document).ready(function() {
     var wrap = $('#wrap');
     var boxes = $('#wrap').children('div');
-    jquery_waterfall(wrap,boxes);
+    jquery_waterfall(wrap, boxes);
+
+    $(this).scroll(function (event) {
+        //检查滚轮位置
+        if(checkAppend(wrap)) {
+            appendBox(wrap, boxes);
+        }
+    })
 })
-
-
-
 
